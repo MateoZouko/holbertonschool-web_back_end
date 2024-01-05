72c65f219b5cc44ec6928f4d29f62be89b18ac76
MateoZouko <mateozouko1@gmail.com>
2024-01-05 10:14:01 -0300

    task 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Task 6"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
+    """sum mixed list"""
+    return sum(mxd_list)
