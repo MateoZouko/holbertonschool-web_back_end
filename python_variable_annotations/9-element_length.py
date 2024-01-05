007fd32b9f4f721ba4d5dba67ad67e0fc4d0614a
MateoZouko <mateozouko1@gmail.com>
2024-01-05 13:11:16 -0300

    fix

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 from typing import List, Iterable, Sequence, Tuple
+"""Task 9"""
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Function that return a list of a tuple"""
     return [(i, len(i)) for i in lst]
