20846cd79e250fb160c0135e721b36667ac19c1b
MateoZouko <mateozouko1@gmail.com>
2024-01-07 10:27:10 -0300

    fixing pycode

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -7,13 +7,14 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """async wait_n"""
     lista = []
-    
-    
+
     for _ in range(n):
         delay = await wait_random(max_delay)
         lista.append(delay)
+
     sorted(lista)
     return lista
