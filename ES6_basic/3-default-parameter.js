ff736a15f97933eaccd1f523a7ddc4646f06d064
Mateo Zouko <mateozouko1@gmail.com>
2024-01-15 21:02:04 +0000

    feat

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -2,6 +2,6 @@ const getSumOfHoods = (
   initialNumber,
   expansion1989 = 89,
   expansion2019 = 19,
-  ) => initialNumber + expansion1898 + expansion2019;
+  ) => initialNumber + expansion1989 + expansion2019;
 
   export default getSumOfHoods;
