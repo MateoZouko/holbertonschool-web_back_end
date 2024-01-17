efbeaadb758fad0c3522fa626b1199c0682de8fd
Mateo Zouko <mateozouko1@gmail.com>
2024-01-17 17:27:31 +0000

    task 5

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,5 +1,4 @@
 export default function concatArrays(array1, array2, string) {
-  let array3 = string.split('')
-  const concatArray = [...array1, ...array2, ...array3]
-	return concatArray
+  const concatArray = [...array1, ...array2, ...string];
+  return concatArray;
 }
