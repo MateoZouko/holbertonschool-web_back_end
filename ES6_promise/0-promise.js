587d9990ac061ddde174ffe6ccd0554e78cbf4b6
MateoZouko <mateozouko1@gmail.com>
2024-01-26 20:33:46 -0300

    task 0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,4 +1,11 @@
 function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
+    const exito = true;
+
+    if (exito) {
+      resolve(true);
+    } else {
+      reject(false);
+    }
   });
 }
