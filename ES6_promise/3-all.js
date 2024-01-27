2db3e0c11a7de12ed4b22664b1434282ade69252
MateoZouko <mateozouko1@gmail.com>
2024-01-27 12:02:11 -0300

    feat

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,14 +1,10 @@
-import { uploadPhoto, createUser } from './utils';
-
+import { uploadPhoto, createUser } from './utils.js';
 export default function handleProfileSignup() {
   const promise1 = uploadPhoto();
   const promise2 = createUser();
   Promise.all([promise1, promise2])
-    .then((result) => {
-      let [result1, result2] = result;
+    .then((results) => {
+      let [result1, result2] = results;
       console.log(`${result1.body} ${result2.firstName} ${result2.lastName}`);
     })
-    .catch(() => {
-      console.error('body firstName lastName');
-    });
 }
