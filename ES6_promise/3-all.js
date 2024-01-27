bde99f45c6a5a98c0e8df943c140cd05faa101e5
MateoZouko <mateozouko1@gmail.com>
2024-01-27 12:20:24 -0300

    task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,10 +1,16 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
+
 export default function handleProfileSignup() {
   const promise1 = uploadPhoto();
   const promise2 = createUser();
   Promise.all([promise1, promise2])
-    .then((results) => {
-      let [result1, result2] = results;
-      console.log(`${result1.body} ${result2.firstName} ${result2.lastName}`);
+    .then(([photoResult, userResult]) => {
+      const { body } = photoResult;
+      const { firstName, lastName } = userResult;
+      const text = (`${body} ${firstName} ${lastName}`);
+      console.log(text);
     })
+    .catch(() => {
+      console.error('Signup system offline');
+    });
 }
