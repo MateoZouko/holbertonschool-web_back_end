60ba763a36df0c5b15ce27de541011f9f5f538ed
MateoZouko <mateozouko1@gmail.com>
2024-01-26 22:06:05 -0300

    task 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,14 +1,11 @@
-export default function handleResponseFromAPI(promise) {
-  promise
-  .then(() => {
-    console.log('Got a response from the API');
-  return {
-        status: 200,
-        body: 'success',
-  };
-})
-.catch(() => {
-    console.log('Got a response from the API');
-    return new Error();
+export default function handleResponseFromAPI(myPromise) {
+  myPromise
+    .then(() => {
+      console.log('Got a response from the API');
+      return {status: 200, body: 'success'};
     })
-  };
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    })
+};
