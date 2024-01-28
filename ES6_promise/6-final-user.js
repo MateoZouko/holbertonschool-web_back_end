2afd4879f298d011f01c63ccb587d700c87d9c60
MateoZouko <mateozouko1@gmail.com>
2024-01-28 20:27:50 -0300

    task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,24 +1,10 @@
-import { signUpUser } from './4-user-promise';
-import { uploadPhoto } from './5-photo-reject';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
-  return Promise.all([signUpPromise, uploadPromise])
-    .then(([signUpResult, uploadResult]) => {
-      return results.map((result) => {
-        return [
-          { status: 'fulfilled', value: signUpResult },
-          { status: 'fulfilled', value: uploadResult },
-        ];
-      });
-    })
-    .catch((error) => {
-      return [
-        {
-          status: 'rejected',
-          value: error,
-        },
-      ];
-    });
+  return Promise.allSettled([
+    uploadPhoto(fileName),
+    signUpUser(firstName, lastName)
+  ])
+    .then((result) => (result));
 }
